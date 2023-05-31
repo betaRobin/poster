2586cb933672581adccea062ed0a600cc0793018
betaRobin <123robinabc@gmail.com>
2023-05-31 14:18:11 +0700

    Added uuid, delete and edit APIs

--- main.go
+++ main.go
@@ -2,109 +2,176 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"net/http"
-	// "database/sql"
-    // _ "github.com/lib/pq"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 const (
-    host     = "localhost"
-    port     = 5432
-    user     = "robinwantah"
-    password = "RobinWantah369"
-    dbname   = "postgres"
+// host     = "localhost"
+// port     = 5432
+// user     = "robinwantah"
+// password = "RobinWantah369"
+// dbname   = "postgres"
 )
 
 var credentials = map[string]string{
-	"UserOne" : "PwOne",
-	"UserTwo" : "PwTwo",
+	"UserOne": "PwOne",
+	"UserTwo": "PwTwo",
 }
 
 type credential struct {
-	Username	string	`json:"username"`
-	Password	string	`json:"password"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
-type note struct {
-	Title		string	`json:"title"`
-	Description	string	`json:"description"`
-	Username	string	`json:"username"`
+type post struct {
+	Id          uuid.UUID `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Username    string    `json:"username"`
 }
 
-var notes = [] note{
-	{Title: "Title One", Description: "Title description", Username: "UserOne"},
-	{Title: "Title Two", Description: "Title description", Username: "UserOne"},
-	{Title: "Title One", Description: "Title description", Username: "UserTwo"},
+var posts = []post{
+	{Id: uuid.New(), Title: "Title One", Description: "Title description", Username: "UserOne"},
+	{Id: uuid.New(), Title: "Title Two", Description: "Title description", Username: "UserOne"},
+	{Id: uuid.New(), Title: "Title One", Description: "Title description", Username: "UserTwo"},
 }
 
-func postLogin(c *gin.Context) {
+func login(c *gin.Context) {
 	var loginCredentials credential
-
 	c.BindJSON(&loginCredentials)
 
 	password, exists := credentials[loginCredentials.Username]
 
-	if (!exists) || (password != loginCredentials.Password){
+	if (!exists) || (password != loginCredentials.Password) {
 		fmt.Println("Username or Password incorrect")
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"status": "400",
+			"status":  "400",
 			"message": "Username or Password incorrect",
 		})
 	} else {
 		fmt.Println("Login success")
 		c.JSON(http.StatusOK, gin.H{
-			"status": "200",
+			"status":  "200",
 			"message": "Success",
 		})
 	}
 }
 
-func postRegister(c *gin.Context) {
+func register(c *gin.Context) {
 	var signUpCredentials credential
-
 	c.BindJSON(&signUpCredentials)
 
 	credentials[signUpCredentials.Username] = signUpCredentials.Password
 
 	fmt.Println("Registration success")
 	c.JSON(http.StatusOK, gin.H{
-		"status": "200",
+		"status":  "200",
 		"message": "Success",
 	})
 }
 
-func postNote(c *gin.Context) {
-	var newNote note
+func createPost(c *gin.Context) {
+	var newPost post
+	c.BindJSON(&newPost)
 
-	c.BindJSON(&newNote)
+	newPost.Id = uuid.New()
 
-	notes = append(notes, newNote)
-	fmt.Println("Note created")
+	posts = append(posts, newPost)
+	fmt.Println("Post created")
 	c.JSON(http.StatusOK, gin.H{
-		"status": "200",
+		"status":  "200",
 		"message": "Success",
 	})
 }
 
-type getNotesRequest struct {
-	Username	string	`json:"username"`
+type getPostsRequest struct {
+	Username string `json:"username"`
 }
 
-func getNotesByUser(c *gin.Context) {
-	var request getNotesRequest
-
+func getPostsByUser(c *gin.Context) {
+	var request getPostsRequest
 	c.BindJSON(&request)
 
-	var userNotes = []note{}
+	var userPosts = []post{}
 
-	for _, note := range notes {
-		if note.Username == request.Username {
-			userNotes = append(userNotes, note)
+	for _, post := range posts {
+		if post.Username == request.Username {
+			userPosts = append(userPosts, post)
 		}
 	}
 
-	c.JSON(http.StatusOK, userNotes)
+	c.JSON(http.StatusOK, userPosts)
+}
+
+type updatePostRequest struct {
+	Username    string    `json:"username"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	PostId      uuid.UUID `json:"post_id"`
+}
+
+func editPost(c *gin.Context) {
+	var request updatePostRequest
+	c.BindJSON(&request)
+
+	if request.Title == "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"status":  "400",
+			"message": "Post Title cannot be empty",
+		})
+	}
+
+	if request.Description == "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"status":  "400",
+			"message": "Post Description cannot be empty",
+		})
+	}
+
+	for _, post := range posts {
+		if post.Id == request.PostId {
+			c.JSON(http.StatusOK, post)
+		}
+	}
+
+	c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+		"status":  "404",
+		"message": "Post not found",
+	})
+}
+
+type deletePostRequest struct {
+	Username string    `json:"username"`
+	PostId   uuid.UUID `json:"post_id"`
+}
+
+func deletePost(c *gin.Context) {
+	var request deletePostRequest
+	c.BindJSON(&request)
+
+	for _, post := range posts {
+		if post.Id == request.PostId {
+			if post.Username == request.Username {
+				c.JSON(http.StatusOK, gin.H{
+					"status":  "200",
+					"message": "Success",
+				})
+			} else {
+				c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
+					"status":  "403",
+					"message": "Forbidden",
+				})
+			}
+		}
+	}
+
+	c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+		"status":  "404",
+		"message": "Post not found",
+	})
 }
 
 func main() {
@@ -113,9 +180,20 @@ func main() {
 	// defer db.Close()
 
 	router := gin.Default()
-	router.POST("/login", postLogin)
-	router.POST("/register", postRegister)
-	router.POST("/post", postNote)
-	router.GET("/all", getNotesByUser)
+
+	// POST
+	router.POST("/login", login)
+	router.POST("/register", register)
+	router.POST("/post", createPost)
+
+	// GET
+	router.GET("/all", getPostsByUser)
+
+	// PUT
+	router.PUT("/edit", editPost)
+
+	// Delete
+	router.DELETE("/delete", deletePost)
+
 	router.Run("localhost:8080")
 }
