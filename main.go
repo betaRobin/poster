5f364ae58f394a8ecf8893b65497f6c2f6761036
betaRobin <123robinabc@gmail.com>
2023-10-05 14:50:54 +0700

    Revert "Undo rebase"
    This reverts commit a91e6d27e2fa71b005080e1594ba4dda12cdbdc2.

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+
+	"github.com/betarobin/poster/controller/post"
+	"github.com/betarobin/poster/controller/user"
+	"github.com/betarobin/poster/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// logging setup
+	log.SetFlags(0)
+	log.SetOutput(new(middleware.LogWriter))
+
+	router := gin.New()
+
+	// middlewares
+	router.Use(gin.Recovery())
+	router.Use(middleware.Logger())
+
+	// user
+	router.POST("/login", user.Login)
+	router.POST("/register", user.Register)
+
+	// post
+	router.POST("/post", post.CreatePost)
+	router.GET("/all", post.GetPostsByUser)
+	router.PUT("/edit", post.EditPost)
+	router.DELETE("/delete", post.DeletePost)
+
+	router.Run("localhost:8080")
+}
+
+// rebase
